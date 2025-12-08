b1bde4f227422bfec41051ec62f8ed0a0ea48a15
Nikolas Sena <nikolas.aguiar.sena@gmail.com>
2025-12-08 14:29:00 +0000

    feat(scripts): Adiciona scripts de instalação

---
+++ arch-setup/archiso-setup.sh
@@ -0,0 +1,137 @@
+#!/bin/bash
+# archiso-setup.sh - Instalação automatizada do Arch Linux via Arch ISO
+
+# Erro na quebra de linhas de senhas erradas
+
+CONTINUE_ON_ERROR=false
+[[ "$1" == "--continue-on-error" ]] && CONTINUE_ON_ERROR=true
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+LINKS="https://raw.githubusercontent.com/nkzr4/nk-dots/refs/heads/nkzr4-arch-setup/arch-install/links.sh"
+curl -LO $LINKS
+chmod +x $SCRIPT_DIR/links.sh
+source $SCRIPT_DIR/links.sh
+
+curl -LO $LINKLOGS
+chmod +x $SCRIPT_DIR/logs.sh
+source $SCRIPT_DIR/logs.sh
+
+run curl -LO $LINKVALIDATIONS
+run chmod +x $SCRIPT_DIR/validations.sh
+source $SCRIPT_DIR/validations.sh
+
+run curl -LO $LINKCHROOT
+run chmod +x /root/chroot-setup.sh
+
+run curl -LO $LINKFIRSTINIT
+run chmod +x /root/first-init.sh
+
+run curl -LO $LINKHYPRCONF
+
+set -euo pipefail
+
+show_header "INICIANDO INSTALAÇÃO DO ARCH LINUX"
+
+validate_internet
+validate_scripts
+
+service_start() {
+    log_info "Iniciando coleta de dados..."
+    show_header "ETAPA 1 - COLETA DE DADOS"
+    run validate_kblayout
+    run validate_timezone
+    run validate_language
+    run validate_pcname
+    run validate_username
+    run validate_rootpasswd
+    run validate_diskname
+    run validate_overview
+
+    log_info "Atualizando Arch ISO.."
+    pacman -Syy --noconfirm
+}
+
+service_disk() {
+    log_info "Verificando tipo do disco.."
+    if [[ $(cat /sys/block/$(basename "$DISK")/queue/rotational) -eq 0 ]]; then
+        ssdIfSsd=",ssd"
+    else
+        ssdIfSsd=""
+    fi
+    log_info "Criando tabela GPT e partições.."
+    run sgdisk --zap-all $DISK
+    run sgdisk -n 1:0:+1024M -t 1:EF00 $DISK
+    run sgdisk -n 2:0:0 -t 2:8300 $DISK
+    log_success "Partições '$DISKNAME1' e '$DISKNAME2' criadas com sucesso.."
+    log_info "Configurando criptografia LUKS na partição Linux.."
+    run echo -n "$LUKSPASSWD" > /tmp/keyfile
+    run chmod 600 /tmp/keyfile
+    run cryptsetup luksFormat "$DISKNAME2" --batch-mode --key-file=/tmp/keyfile
+    run cryptsetup luksOpen "$DISKNAME2" main --key-file=/tmp/keyfile
+    run rm -f /tmp/keyfile
+    log_success "Partição '$DISKNAME2' criptografada com sucesso.."
+    log_info "Formatando partição '$DISKNAME2' como Btrfs.."
+    run mkfs.btrfs /dev/mapper/main
+    log_success "Partição '$DISKNAME2' formatada com sucesso.."
+    log_info "Criando subvolumes Btrfs.."
+    run mount /dev/mapper/main /mnt
+    run cd /mnt
+    run btrfs subvolume create @
+    run btrfs subvolume create @home
+    run btrfs subvolume create @snapshots
+    run btrfs subvolume list /mnt
+    cd
+    run umount /mnt
+    log_success "Subvolumes Btrfs criados com sucesso.."
+    log_info "Montando subvolumes.."
+    run mount -o noatime$ssdIfSsd,compress=zstd,space_cache=v2,discard=async,subvol=@ /dev/mapper/main /mnt
+    run mkdir /mnt/home
+    run mount -o noatime$ssdIfSsd,compress=zstd,space_cache=v2,discard=async,subvol=@home /dev/mapper/main /mnt/home
+    run mkdir /mnt/.snapshots
+    run mount -o noatime$ssdIfSsd,compress=zstd,space_cache=v2,discard=async,subvol=@snapshots /dev/mapper/main /mnt/.snapshots
+    run findmnt -t btrfs
+    log_success "Subvolumes montados com sucesso.."
+    log_info "Montando partição de boot.."
+    run mkfs.fat -F32 $DISKNAME1
+    run mkdir /mnt/boot
+    run mount $DISKNAME1 /mnt/boot
+    log_success "Boot montado com sucesso.."
+    log_info "Iniciando preparação de disco..."
+    show_header "ETAPA 3 - EXECUTANDO BOOTSTRAP"
+    log_info "Instalando pacotes base.."
+    run pacstrap /mnt base linux linux-headers linux-firmware nano btrfs-progs grub efibootmgr --noconfirm
+    log_success "Pacotes instalados com sucesso.."
+    log_info "Gerando fstab.."
+    run genfstab -U -p /mnt > /mnt/etc/fstab
+    log_success "fstab gerado com sucesso.."
+}
+
+cd
+show_header "nk-dots - ARCH LINUX BTRFS"
+run service_start
+
+show_header "ETAPA 2 - PREPARANDO DISCO BTRFS"
+run service_disk
+
+show_header "ETAPA 3 - ENTRANDO EM CHROOT"
+log_info "Preparando scripts.."
+run mv /root/chroot-setup.sh /mnt/chroot-setup.sh
+run mv /root/first-init.sh /mnt/first-init.sh
+run mv /root/hyprland.conf.default /mnt/hyprland.conf.default
+run cp /root/logs.sh /mnt/logs.sh
+run cp /root/links.sh /mnt/links.sh
+run cp /root/vars.sh /mnt/vars.sh
+log_success "Scripts gerados com sucesso.."
+run arch-chroot /mnt /bin/bash -c "/chroot-setup.sh"
+
+show_header "INSTALAÇÃO DO ARCH LINUX FINALZIADA"
+run rm $SCRIPT_DIR/links.sh
+run rm $SCRIPT_DIR/logs.sh
+run rm $SCRIPT_DIR/validations.sh
+run rm $SCRIPT_DIR/vars.sh
+run rm /mnt/chroot-setup.sh
+run rm /mnt/vars.sh
+run rm /mnt/logs.sh
+run rm /mnt/links.sh
+run reboot
---
+++ arch-setup/chroot-setup.sh
@@ -0,0 +1,115 @@
+#!/bin/bash
+# chroot-setup.sh - Preparação do chroot via Arch ISO
+
+source /logs.sh
+source /vars.sh
+source /links.sh
+
+set -euo pipefail
+
+service_user() {
+    log_info "Definindo fuso horário.."
+    run ln -sf /usr/share/zoneinfo/$TIMEZONE /etc/localtime
+    run hwclock --systohc
+    log_success "Fuso horário definido como: $TIMEZONE.."
+    log_info "Definindo idioma.."
+    run sed -i "s/^#\($LANGUAGE\)/\1/" /etc/locale.gen
+    run locale-gen
+    run echo "LANG=$LANGUAGE" >> /etc/locale.conf
+    log_success "Idioma definido como: $LANGUAGE.."
+    log_info "Definindo layout do teclado.."
+    run echo "$KBLAYOUT" >> /etc/vconsole.conf
+    log_success "Layout definido como: $KBLAYOUT.."
+    log_info "Definindo nome do computador.."
+    run echo "$PCNAME" >> /etc/hostname
+    log_success "Nome do computador definido como: $PCNAME.."
+    log_info "Definindo senha do root.."
+    run echo -e "$ROOTPASSWD\n$ROOTPASSWD" | passwd root
+    log_success "Senha do root definida com sucesso.."
+    log_info "Criando usuário.."
+    run useradd -m -g users -G wheel $USERNAME
+    log_success "Usuário '$USERNAME' criado com sucesso.."
+    log_info "Definindo senha de $USERNAME.."
+    run echo -e "$USERPASSWD\n$USERPASSWD" | passwd $USERNAME
+    log_success "Senha de $USERNAME definida com sucesso.."
+    log_info "Garantindo a $USERNAME permissões de root.."
+    run pacman -Sy --noconfirm sudo
+    run echo "$USERNAME ALL=(ALL) ALL" >> /etc/sudoers.d/$USERNAME
+    log_success "Permissões garantidas com sucesso.."
+}
+
+service_installer() {
+    log_info "Preparando repositórios.."
+    run curl -O https://download.sublimetext.com/sublimehq-pub.gpg
+    run pacman-key --add sublimehq-pub.gpg
+    run pacman-key --lsign-key 8A8F901A
+    run rm sublimehq-pub.gpg
+    run echo -e "\n[sublime-text]\nServer = https://download.sublimetext.com/arch/stable/x86_64" >> /etc/pacman.conf
+    run sed -i 's/^#\[multilib\]/[multilib]/' /etc/pacman.conf
+    run sed -i '/^\[multilib\]/{n;s/^#Include/Include/}' /etc/pacman.conf	
+    log_success "Repositórios adicionados com sucesso.."
+    log_info "Definindo vendor do processador.."
+    CPU_VENDOR=$(lscpu | grep "Vendor ID" | awk '{print $3}')
+    if [[ $CPU_VENDOR == "GenuineIntel" ]]; then
+        cpu="intel-ucode"
+    else
+        cpu="amd-ucode"
+    fi
+    log_success "Vendor definido como '$CPU_VENDOR'.."
+    log_info "Iniciando instalação.."
+    run pacman -Sy --noconfirm base-devel grub-btrfs mtools networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools openssh git pipewire pipewire-pulse pipewire-jack wireplumber bluez bluez-utils xdg-utils xdg-user-dirs alsa-utils inetutils $cpu man-db man-pages texinfo ipset firewalld acpid hyprland dunst kitty uwsm thunar xdg-desktop-portal-hyprland qt5-wayland qt6-wayland polkit-kde-agent grim slurp noto-fonts ttf-font-awesome firefox vlc vlc-plugins-all okular sublime-text spotify-launcher discord steam libreoffice-fresh qbittorrent virtualbox virtualbox-host-modules-arch inotify-tools fish gnome-calculator obs-studio bash-completion
+    log_success "Aplicações e dependências instaladas sucesso.."
+    log_info "Ativando serviços.."
+    run systemctl enable NetworkManager
+    run systemctl enable bluetooth
+    run systemctl enable sshd
+    run systemctl enable firewalld
+    run systemctl enable fstrim.timer
+    run systemctl enable acpid
+    log_success "Serviços ativados com sucesso.."
+}
+
+service_boot() {
+    log_info "Configurando mkinitcpio.conf.."
+    run sed -i 's/^MODULES=.*/MODULES=(btrfs)/' /etc/mkinitcpio.conf
+    run sed -i '/^HOOKS=/ s/filesystems/sd-encrypt filesystems/' /etc/mkinitcpio.conf
+    run mkinitcpio -p linux
+    log_success "mkinitcpio concluído com sucesso.."
+    log_info "Configurando GRUB.."
+    run grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+    run grub-mkconfig -o /boot/grub/grub.cfg
+    DISK_LUKS_UUID=$(run blkid -s UUID -o value $DISKNAME2)
+    run sed -i "s|GRUB_CMDLINE_LINUX_DEFAULT=\"loglevel=3 quiet\"|GRUB_CMDLINE_LINUX_DEFAULT=\"loglevel=3 quiet rd.luks.name=$DISK_LUKS_UUID=main root=/dev/mapper/main rootflags=subvol=@\"|" /etc/default/grub
+    run grub-mkconfig -o /boot/grub/grub.cfg
+    log_success "GRUB configurado com sucesso.."
+    log_info "Configurando script de primeira inicialização.."
+    run mkdir -p /home/$USERNAME/.config/{nk-dots,hypr}
+    run mv /first-init.sh /home/$USERNAME/.config/nk-dots/first-init.sh
+    run chmod +x /home/$USERNAME/.config/nk-dots/first-init.sh
+    run cp /logs.sh /home/$USERNAME/.config/nk-dots/logs.sh
+    run chmod +x /home/$USERNAME/.config/nk-dots/logs.sh
+    log_success "Script preparado com sucesso.."
+    log_info "Preparando inicialização do Hyprland..."
+    run mv /hyprland.conf.default /home/$USERNAME/.config/hypr/hyprland.conf
+cat >> /home/$USERNAME/.bash_profile << 'EOF'
+if [ -z "$DISPLAY" ] && [ "$XDG_VTNR" -eq 1 ]; then
+    exec Hyprland
+fi
+EOF
+    chown $USERNAME:wheel /home/$USERNAME/.bash_profile
+    log_success "Configuração do hyprland criada com sucesso.."
+    run chown -R $USERNAME:wheel /home/$USERNAME/.config
+    log_info "Saindo de ambiente chroot.."
+}
+
+cd
+show_header "ETAPA 4 - CONFIGURAÇÕES DO USUÁRIO"
+run service_user
+
+show_header "ETAPA 5 - INSTALANDO APLICAÇÕES"
+run service_installer
+
+show_header "ETAPA 6 - PREPARANDO INICIALIZAÇÃO"
+run service_boot
+
+exit
