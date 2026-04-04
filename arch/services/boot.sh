c4fc6d79901780a9693cc0539fc596d17d7d2161
Nikolas Sena <60147254+nkzr4@users.noreply.github.com>
2026-04-04 17:09:14 -0300

    feature(autostart): Define inicialização automatica de shell.sh

--- arch/services/boot.sh
+++ arch/services/boot.sh
@@ -10,7 +10,12 @@ config_boot() {
     run set_mkinitcpio_conf
     run set_grub_conf
     run set_repos
+    run set_autohypr
     run set_autologin
+    run set_iwd_to_nm
+    if [ "$NMCLI_SET" = "true" ]; then
+        run set_startshell
+    fi
 }
 
 set_mkinitcpio_conf() {
@@ -45,6 +50,18 @@ set_repos() {
     chmod +x /home/$VAR_USERNAME/.config/nk-dots/arch/shell.sh
 }
 
+set_autohypr() {
+    log_info "Preparando inicialização do hyprland"
+    [[ -f /home/$VAR_USERNAME/.config/nk-dots/hypr/hyprland.conf.default ]] || fatal "hyprland.conf.default não encontrado"
+    sed -i -E "s/\bUSERNAME\b/$VAR_USERNAME/g" /home/$VAR_USERNAME/.config/nk-dots/hypr/hyprland.conf.default || fatal "Falha ao configurar hyprland.conf"
+    grep -q "$VAR_USERNAME" /home/$VAR_USERNAME/.config/nk-dots/hypr/hyprland.conf.default || fatal "Username não foi aplicado no Hyprland conf"
+    mkdir -p /home/$VAR_USERNAME/.config/hypr
+    cp /home/$VAR_USERNAME/.config/nk-dots/hypr/hyprland.conf.default /home/$VAR_USERNAME/.config/hypr/hyprland.conf
+    cat_bash_profile
+    [[ -f /home/$VAR_USERNAME/.bash_profile ]] || fatal ".bash_profile não criado"
+    chown $VAR_USERNAME:wheel /home/$VAR_USERNAME/.bash_profile
+}
+
 set_autologin() {
     log_info "Configurando autologin de '$VAR_USERNAME'"
     mkdir -p /etc/systemd/system/getty@tty1.service.d || fatal "Falha ao criar diretório systemd override"
@@ -53,6 +70,74 @@ set_autologin() {
     systemctl enable getty@tty1.service || fatal "Falha ao habilitar autologin"
     systemctl is-enabled getty@tty1.service >/dev/null || fatal "Autologin não está habilitado"
     chown -R $VAR_USERNAME:wheel /home/$VAR_USERNAME/.config
+}
+
+set_startshell() {
+    log_info "Configurando serviço de conclusão de instalação"
+    SERVICE_PATH="/etc/systemd/system/nkshell.service"
+    cat << 'EOF' > "$SERVICE_PATH"
+[Unit]
+Description=Finish nk-dots configuration (first boot)
+After=network.target
+
+[Service]
+Type=oneshot
+ExecStart=/home/USERNAME/.config/nk-dots/arch/shell.sh
+RemainAfterExit=yes
+
+# Remove o próprio serviço após rodar (one-shot real)
+ExecStartPost=/bin/systemctl disable nkshell.service
+
+[Install]
+WantedBy=multi-user.target
+EOF
+    sed -i "s|USERNAME|$VAR_USERNAME|g" "$SERVICE_PATH"
+    chmod 644 "$SERVICE_PATH"
+    systemctl enable nkshell.service
+    log_success "Serviço de execução de script finalizado"
+}
+
+set_iwd_to_nm() {
+    IFACE=$(ip -o -4 route show to default | awk '{print $5}' | head -n1)
+    TYPE=$(cat /sys/class/net/"$IFACE"/type 2>/dev/null)
+    if [ "$TYPE" = "1" ]; then
+        return 0
+    else
+        log_info "Obtendo credenciais de acesso a internet"
+        WIFI_DEV=$(iwctl device list | awk '/station/ {print $2; exit}')
+        if [ -z "$WIFI_DEV" ]; then
+            echo "Nenhuma interface Wi-Fi encontrada."
+            return 1
+        fi
+        SSID=$(iwctl station "$WIFI_DEV" show | awk -F': ' '/Connected network/ {print $2}')
+        if [ -z "$SSID" ]; then
+            echo "Não conectado a nenhuma rede via iwctl."
+            return 1
+        fi
+        IWD_FILE="/var/lib/iwd/${SSID}.psk"
+        if [ ! -f "$IWD_FILE" ]; then
+            echo "Arquivo de credenciais não encontrado: $IWD_FILE"
+            return 1
+        fi
+        PASSWORD=$(grep -i '^PreSharedKey=' "$IWD_FILE" | cut -d= -f2)
+        if [ -z "$PASSWORD" ]; then
+            echo "Não foi possível extrair a senha."
+            return 1
+        fi
+        log_info "SSID: $SSID"
+        log_info "Importando para NetworkManager..."
+        nmcli connection add type wifi \
+            ifname "$WIFI_DEV" \
+            con-name "$SSID" \
+            ssid "$SSID" >/dev/null 2>&1
+        nmcli connection modify "$SSID" \
+            wifi-sec.key-mgmt wpa-psk \
+            wifi-sec.psk "$PASSWORD" \
+            connection.autoconnect yes
+        chmod 600 "/etc/NetworkManager/system-connections/$SSID.nmconnection" 2>/dev/null
+        log_sucess "Conexão criada com sucesso no NetworkManager."
+        NMCLI_SET="true"
+    fi
 }
 
 cat_autologin_conf() {
